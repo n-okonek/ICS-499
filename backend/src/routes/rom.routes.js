fe141eab231fcb857721994959fbd3809bd7e8c7
Jack Henry <jack@jackerickson.net>
2021-10-14 16:39:20 -0500

    Created controller, routes, services and validators for rom entity. So far, only listRoms and createRom are supported.

---
+++ backend/src/routes/rom.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import RomController from '../controllers/rom.controller.js';
+import RomValidator from '../validatiors/rom.validators.js';
+import { check } from 'express-validator';
+
+const router = express.Router();
+
+router.get('/', RomController.listRoms);
+router.post('/', RomValidator.createRom, RomController.createRom);
+
+export default router;
