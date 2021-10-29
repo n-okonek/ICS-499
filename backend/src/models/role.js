267a5e722981bafcafaeb3bda9d14850c2190d48
aahm2221 <aisha.ahmed1@yahoo.com>
2021-10-28 21:00:52 -0500

    role sequelize fix

--- backend/src/models/role.js
+++ backend/src/models/role.js
@@ -11,7 +11,7 @@ export default function(sequelize, DataTypes) {
         },
         created: {
             type: DataTypes.DATE,
-            defaultValue: Sequelize.NOW
+            defaultValue: DataTypes.NOW
         }
     }, {
         tableName: 'role'
