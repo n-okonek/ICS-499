b8aecea515c34a01653d89c605d8dc54d4e66ba5
Jack Henry <jack@jackerickson.net>
2021-11-04 16:50:12 -0500

    added valid_until field to session so keys can be invalidated

--- backend/src/models/session.js
+++ backend/src/models/session.js
@@ -1,4 +1,4 @@
-import db from '../config/db.js';
+import moment from 'moment';
 
 export default function(sequelize, DataTypes) {
     const session = sequelize.define('session', {
@@ -6,8 +6,15 @@ export default function(sequelize, DataTypes) {
             type: DataTypes.STRING,
             primaryKey: true,
             allowNull: false
+        },
+        valid_until: {
+            type: DataTypes.DATE,
         }
     });
+    
+    session.addHook('afterCreate', (new_session, options) => {
+        new_session.valid_until = moment().add(1, 'days').toDate()
+    })
     
     session.associate = models => {
         session.belongsTo(models.user, {
