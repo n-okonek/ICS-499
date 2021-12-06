5676898e60d9c2a4989d85de69d44e98a6a720de
n-okonek <41804767+n-okonek@users.noreply.github.com>
2021-12-05 21:46:31 -0600

    restrict my account and admin links to only authenticated users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sass/styles.scss';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import OuterScreenView from './views/pages/home/OuterScreenView';
 import Login from './views/pages/login/Login';
@@ -11,16 +12,31 @@ import UserManagement from './views/pages/admin/UserManagement';
 import RomManagement from './views/pages/admin/RomManagement';
 
 function App() {
+  const loggedIn = useSelector((state) => state.login.loggedIn);
+  const userRole = useSelector((state) => state.login.userRole);
+
   return (
     <Router>
       <div>
         <Route exact path="/" component={OuterScreenView} />
         <Route path="/login" component={Login} />
-        <Route path="/create-account" component={CreateAccount} />
-        <Route path="/user/library" component={Library} />
-        <Route path="/user/profile" component={Profile} />
-        <Route path="/admin/user-management" component={UserManagement} />
-        <Route path="/admin/rom-management" component={RomManagement} />
+        {loggedIn ? (
+          <>
+            <Route path="/create-account" component={CreateAccount} />
+            <Route path="/user/library" component={Library} />
+            <Route path="/user/profile" component={Profile} />
+            {loggedIn && userRole === "admin" ? (
+              <>
+                <Route path="/admin/user-management" component={UserManagement} />
+                <Route path="/admin/rom-management" component={RomManagement} />
+              </>
+            ) : (
+              <Redirect exact to="/login" component={Login} />
+            )}
+          </>
+        ) : (
+          <Redirect exact to="/login" component={Login} />
+        )}
       </div>
     </Router>
   );
