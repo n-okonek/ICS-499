0191ed24d45b0f309e94c7622cac93f5a065b3f4
Jack Henry <jackhenry.tech@gmail.com>
2021-12-08 22:12:01 -0600

    Fixed environmnet variable issue.

--- backend/src/config/environment.js
+++ backend/src/config/environment.js
@@ -1,8 +1,8 @@
 // Load the environment variables
 import dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV === 'test' ? `.env.test` : '.env'
-const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : envFile;
+let envFile = process.env.NODE_ENV === 'test' ? `.env.test` : '.env'
+envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : envFile;
 
 console.debug(`Loading enviornment variables from file ${envFile}`);
 
