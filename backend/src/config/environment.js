7c45fb93e9403c615ae5222ce4a9d6040d19809a
Jack Henry <jackhenry.tech@gmail.com>
2021-12-08 22:03:09 -0600

    Fixed environment variables in deploy configuration.

--- backend/src/config/environment.js
+++ backend/src/config/environment.js
@@ -2,6 +2,7 @@
 import dotenv from 'dotenv';
 
 const envFile = process.env.NODE_ENV === 'test' ? `.env.test` : '.env'
+const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : envFile;
 
 console.debug(`Loading enviornment variables from file ${envFile}`);
 
