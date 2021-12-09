2fe13df57868861f0a077d5f775201870b67e313
Jack Henry <jackhenry.tech@gmail.com>
2021-12-09 11:53:53 -0600

    NODE_ENV is set to production if unset

--- backend/src/config/environment.js
+++ backend/src/config/environment.js
@@ -1,6 +1,10 @@
 // Load the environment variables
 import dotenv from 'dotenv';
 
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'production';
+}
+
 let envFile = process.env.NODE_ENV === 'test' ? `.env.test` : '.env'
 envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : envFile;
 
