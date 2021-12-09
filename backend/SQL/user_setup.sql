2f0f96cc97c559de8e6776b27d319b11b5e4a3a4
Jack Henry <jackhenry.tech@gmail.com>
2021-12-09 11:24:06 -0600

    Only deploy backend

--- backend/SQL/user_setup.sql
+++
@@ -1,4 +0,0 @@
---Queries I used to create eaas user. App expects there to be a user named eaas with read/write privileges
-CREATE USER 'eaas'@'localhost' IDENTIFIED BY 'bluefish';
-GRANT ALL PRIVILEGES ON eaas.* TO 'eaas'@'localhost';
-FLUSH PRIVILEGES;
